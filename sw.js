e7432673fa7563beb9e3c6e0baaf5422bde2482a
septem-sensu <info@septem-sensu.de>
2026-07-15 13:08:00 +0200

    Reambuilding Regel in die Dokumentation integriert

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME      = 'friendshunt-v0.1.0.47';
+const CACHE_NAME      = 'friendshunt-v0.1.0.49';
 const TILE_CACHE_NAME = 'friendshunt-tiles-v1';
 const TILE_MAX_AGE_MS = 2 * 60 * 60 * 1000; // 2 hours
 
